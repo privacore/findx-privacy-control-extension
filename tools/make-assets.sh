219258933bdc5eb63ed76f449a66f73b195750ac
Brian Rasmusson <br@privacore.com>
2018-04-05 12:08:44 +0200

    keep uAssets in current working directory and clean up/clone again on each build

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -7,9 +7,13 @@ DES=$1/assets
 printf "*** Packaging assets in $DES... "
 
 #if [ -n "${TRAVIS_TAG}" ]; then
-  pushd .. > /dev/null
+  rm -rf .uassets
+  mkdir .uassets
+  cd .uassets
+#  pushd .. > /dev/null
   git clone --depth 1 https://github.com/uBlockOrigin/uAssets.git
-  popd > /dev/null
+  cd ..
+#  popd > /dev/null
 #fi
 
 rm -rf $DES
@@ -17,14 +21,14 @@ mkdir $DES
 cp    ./assets/assets.json                                       $DES/
 
 mkdir $DES/thirdparties
-cp -R ../uAssets/thirdparties/easylist-downloads.adblockplus.org $DES/thirdparties/
-cp -R ../uAssets/thirdparties/mirror1.malwaredomains.com         $DES/thirdparties/
-cp -R ../uAssets/thirdparties/pgl.yoyo.org                       $DES/thirdparties/
-cp -R ../uAssets/thirdparties/publicsuffix.org                   $DES/thirdparties/
-cp -R ../uAssets/thirdparties/www.malwaredomainlist.com          $DES/thirdparties/
+cp -R .uassets/uAssets/thirdparties/easylist-downloads.adblockplus.org $DES/thirdparties/
+cp -R .uassets/uAssets/thirdparties/mirror1.malwaredomains.com         $DES/thirdparties/
+cp -R .uassets/uAssets/thirdparties/pgl.yoyo.org                       $DES/thirdparties/
+cp -R .uassets/uAssets/thirdparties/publicsuffix.org                   $DES/thirdparties/
+cp -R .uassets/uAssets/thirdparties/www.malwaredomainlist.com          $DES/thirdparties/
 
 mkdir $DES/ublock
-cp -R ../uAssets/filters/*                                       $DES/ublock/
+cp -R .uassets/uAssets/filters/*                                       $DES/ublock/
 # Optional filter lists: do not include in package
 rm    $DES/ublock/annoyances.txt
 
