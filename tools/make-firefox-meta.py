fcb4d33b2a61a38ac282a9751e2941261d7a3d22
igor.petrenko <igor.petrenko@smartpelican.com>
2018-08-15 17:40:59 +0300

    Fixes for Firefox building

--- tools/make-firefox-meta.py
+++ tools/make-firefox-meta.py
@@ -20,15 +20,17 @@ firefox_manifest_file = os.path.join(build_dir, 'manifest.json')
 with open(firefox_manifest_file) as f2:
     firefox_manifest = json.load(f2)
 
-match = re.search('^(\d+\.\d+\.\d+)(\.\d+)$', version)
-if not match:
+#match = re.search('^(\d+\.\d+\.\d+)(\.\d+)$', version)
+#if not match:
     # https://bugzilla.mozilla.org/show_bug.cgi?id=1459007
     # By design Firefox opens the sidebar with new installation of
     # uBO when sidebar_action is present in the manifest.
     # Remove sidebarAction support for stable release of uBO.
-    del firefox_manifest['sidebar_action']
+    #del firefox_manifest['sidebar_action']
 
 firefox_manifest['version'] = version
+print(version)
+print(firefox_manifest['version'])
 
 with open(firefox_manifest_file, 'w') as f2:
     json.dump(firefox_manifest, f2, indent=2, separators=(',', ': '), sort_keys=True)
